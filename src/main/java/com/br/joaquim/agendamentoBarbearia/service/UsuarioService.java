c5e7ef8d5c0d98833efcc3ab5a69d9c7511de1f9
Joaquimat <joaquimmatheussantos@gmail.com>
2026-09-15 20:03:20 -0300

    método para visualizar agendamento do usuário adicionado no service

--- src/main/java/com/br/joaquim/agendamentoBarbearia/service/UsuarioService.java
+++ src/main/java/com/br/joaquim/agendamentoBarbearia/service/UsuarioService.java
@@ -1,9 +1,12 @@
 package com.br.joaquim.agendamentoBarbearia.service;
 
 import com.br.joaquim.agendamentoBarbearia.Dto.UsuariosDto;
+import com.br.joaquim.agendamentoBarbearia.Entity.AgendamentoEntity;
 import com.br.joaquim.agendamentoBarbearia.Entity.UsuariosEntity;
+import com.br.joaquim.agendamentoBarbearia.Repoaitory.IAgendamentoRepository;
 import com.br.joaquim.agendamentoBarbearia.Repoaitory.IUsuariosRepository;
 import com.br.joaquim.agendamentoBarbearia.exception.BadRequestException;
+import com.br.joaquim.agendamentoBarbearia.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final IUsuariosRepository usuariosRepository;
+    private final IAgendamentoRepository agendamentoRepository;
 
     public void criarUsuario(UsuariosDto usuariosDto) throws BadRequestException {
         UsuariosEntity usuarios = usuariosRepository.findByEmail(usuariosDto.getEmail())
@@ -20,16 +24,21 @@ public class UsuarioService {
             throw new BadRequestException("Usuário já existe");
         }
         usuariosRepository.save(UsuariosEntity.builder()
-                        .nome(usuariosDto.getNome())
-                        .email(usuariosDto.getEmail())
-                        .senha(usuariosDto.getSenha())
+                .nome(usuariosDto.getNome())
+                .email(usuariosDto.getEmail())
+                .senha(usuariosDto.getSenha())
                 .build());
-
     }
 
-
-
-
+    public AgendamentoEntity getUsuarioAgendamento(Integer id) throws NotFoundException {
+        UsuariosEntity usuarios = usuariosRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Usuário não encontrado"));
+        AgendamentoEntity agendamento = usuarios.getAgendamento();
+        if (agendamento == null) {
+            throw new BadRequestException("Agendamento não encontrado para o usuário");
+        }
+        return agendamento;
+    }
 
 
 }
