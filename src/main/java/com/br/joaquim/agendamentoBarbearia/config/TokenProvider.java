d594289bbe656325ac7d40f79f5894c51f8abd75
Joaquimat <joaquimmatheussantos@gmail.com>
2026-09-17 19:26:28 -0300

    token provider adicionado

---
+++ src/main/java/com/br/joaquim/agendamentoBarbearia/config/TokenProvider.java
@@ -0,0 +1,66 @@
+package com.br.joaquim.agendamentoBarbearia.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class TokenProvider {
+
+    @Value("${JWT_EXPIRATION:900000}")
+    private Long expirationTime;
+
+    @Value("${jwt.key}")
+    private String Key;
+
+    String getUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(Key.getBytes());
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
+
+    private String buildToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationTime);
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String gerarToken(Authentication authentication) {
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        return buildToken(user.getUsername());
+    }
+
+}
